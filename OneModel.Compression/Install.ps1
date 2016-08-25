0f960241238d71c976262a02a1c30b137c5118cb
Marcus Graham <m.j.graham93@gmail.com>
2016-08-25 11:10:55 +1000

    Fix for Install.ps1 modification of file properties.

--- OneModel.Compression/Install.ps1
+++ OneModel.Compression/Install.ps1
@@ -1,9 +1,11 @@
 ﻿param($installPath, $toolsPath, $package, $project)
 
 $7zexe = $project.ProjectItems.Item("Dependencies").ProjectItems.Item("7z.exe");
+$7zdll = $project.ProjectItems.Item("Dependencies").ProjectItems.Item("7z.dll");
 $lzopexe = $project.ProjectItems.Item("Dependencies").ProjectItems.Item("lzop.exe");
 $lzo1dll = $project.ProjectItems.Item("Dependencies").ProjectItems.Item("lzo1.dll");
 # Modify CopyOutputToDirectory to Copy If Newer.
 $7zexe.Properties.Item("CopyToOutputDirectory").Value = [int]2;
+$7zdll.Properties.Item("CopyToOutputDirectory").Value = [int]2;
 $lzopexe.Properties.Item("CopyToOutputDirectory").Value = [int]2;
 $lzo1dll.Properties.Item("CopyToOutputDirectory").Value = [int]2;
